cd8033908a240ae74b81dde379e365f8580e3db2
LucasGewl <131786887+LucasGewl@users.noreply.github.com>
2023-10-02 20:46:28 +0000

    "bair tipper hitbox given prio"

--- src/marth/mod.rs
+++ src/marth/mod.rs
@@ -129,7 +129,17 @@ unsafe fn marth_bair(fighter: &mut L2CAgentBase){
     }
     frame(fighter.lua_state_agent,7.0);
     if macros::is_excute(fighter){
-        macros::ATTACK(fighter,0,0,Hash40::new("sword1"),9.0,361,85,0,40,3.5,0.0,0.0,1.7,None,None,None,0.7,1.0,*ATTACK_SETOFF_KIND_ON,*ATTACK_LR_CHECK_F,false,0,0.0,0,false,false,false,false,true,*COLLISION_SITUATION_MASK_GA,*COLLISION_CATEGORY_MASK_ALL,*COLLISION_PART_MASK_ALL,false,Hash40::new("collision_attr_cutup"),*ATTACK_SOUND_LEVEL_M,*COLLISION_SOUND_ATTR_CUTUP,*ATTACK_REGION_SWORD)
+        macros::ATTACK(fighter,2,0,Hash40::new("sword1"),9.0,361,85,0,40,3.5,0.0,0.0,1.7,None,None,None,1.0,1.0,*ATTACK_SETOFF_KIND_ON,*ATTACK_LR_CHECK_F,false,0,0.0,0,false,false,false,false,true,*COLLISION_SITUATION_MASK_GA,*COLLISION_CATEGORY_MASK_ALL,*COLLISION_PART_MASK_ALL,false,Hash40::new("collision_attr_cutup"),*ATTACK_SOUND_LEVEL_M,*COLLISION_SOUND_ATTR_CUTUP,*ATTACK_REGION_SWORD);
+        macros::ATTACK(fighter,1, 0, Hash40::new("armr"), 9.0, 361, 85, 0, 40, 3.5, 2.0, 0.0, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_M, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_SWORD);
+        macros::ATTACK(fighter,0, 0, Hash40::new("sword1"), 12.5, 361, 94, 0, 40, 3.5, 0.0, 0.0, 7.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_cutup"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_MARTH_SWORD, *ATTACK_REGION_SWORD);
+    }
+    sv_animcmd::wait(fighter.lua_state_agent,5);
+    if macros::is_excute{
+        AttackModule::clear_all()
+    }
+    frame(fighter.lua_state_agent, 32.0);
+    if macros::is_excute(fighter){
+        WorkModule::off_flag(fighter.module_accessor,*FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING);
     }
 }
 
