e443948270a06ec59e33bea9d4e0928acb43f4a8
LucasGewl <131786887+LucasGewl@users.noreply.github.com>
2023-10-02 13:37:33 +0000

    "more on marth bair"

--- src/marth/mod.rs
+++ src/marth/mod.rs
@@ -127,6 +127,10 @@ unsafe fn marth_bair(fighter: &mut L2CAgentBase){
         WorkModule::on_flag(fighter.module_accessor, *FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING);
         macros::REVERSE_LR(fighter);
     }
+    frame(fighter.lua_state_agent,7.0);
+    if macros::is_excute(fighter){
+        macros::ATTACK(fighter,0,0,Hash40::new("sword1"),9.0,361,85,0,40,3.5,0.0,0.0,1.7,None,None,None,0.7,1.0,*ATTACK_SETOFF_KIND_ON,*ATTACK_LR_CHECK_F,false,0,0.0,0,false,false,false,false,true,*COLLISION_SITUATION_MASK_GA,*COLLISION_CATEGORY_MASK_ALL,*COLLISION_PART_MASK_ALL,false,Hash40::new("collision_attr_cutup"),*ATTACK_SOUND_LEVEL_M,*COLLISION_SOUND_ATTR_CUTUP,*ATTACK_REGION_SWORD)
+    }
 }
 
 //Down Tilt
